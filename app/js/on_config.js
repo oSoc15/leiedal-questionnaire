04f516bfc0eca2f2f91bb2d95ea4d0cfaf527348
Alana Panny <panny.alana@gmail.com>
2015-07-16 09:47:17 +0200

    created views, linked views

--- app/js/on_config.js
+++ app/js/on_config.js
@@ -7,15 +7,47 @@ function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
 
   $locationProvider.html5Mode(true);
 
+  // set a semicolon at the last .state
+  // otherwise you will get compile errors
   $stateProvider
-      .state('Home', {
+      /*.state('home', {
         //url index
         url: '/',
         controller: 'ExampleCtrl',
         //url view
-        templateUrl: 'home.html'
+        templateUrl: 'home.html',
         //title: 'Home'
-        });
+    })*/
+
+     /*.state('home.helloworld', {
+        url: '/',
+        controller: 'HelloWorldCtrl',
+        templateUrl: 'helloworld.html'
+    });*/
+
+        // HOMEPAGE WITH MULTIPLE NAMED VIEWS
+        .state('home', {
+            url: '/',
+            views: {
+                // HOMEPAGE
+                '': {templateUrl: 'page-home.html'},
+                // DUMMY PAGE -> SAMPLE
+                //'helloworld@home': {templateUrl: 'helloworld.html'}
+            }
+        })
+
+        // FAQPAGE
+        .state('faq', {
+            url: '/veel-gestelde-vragen',
+            templateUrl: 'page-faq.html'
+        })
+
+        // CONTACTPAGE
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'page-contact.html'
+        })
+
 
   $urlRouterProvider.otherwise('/');
 
