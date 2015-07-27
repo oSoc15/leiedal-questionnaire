687dfae357ccc6de5e2851f558d926a2bf4778fb
unknown <panny.alana@gmail.com>
2015-07-27 13:05:00 +0200

    added energietips and controllers to questionnaire page, contact page and faq page

--- app/js/on_config.js
+++ app/js/on_config.js
@@ -8,81 +8,75 @@ function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
 
   // set a semicolon at the last .state
-  // otherwise you will get compile errors
+  // => you will get compile errors
   $stateProvider
-      /*.state('home', {
-        //url index
+    // Homepage with multiple named views
+    .state('home', {
         url: '/',
-        controller: 'ExampleCtrl',
-        //url view
-        templateUrl: 'home.html',
-        //title: 'Home'
-    })*/
+        views: {
+            // HOMEPAGE
+            '': {templateUrl: 'page-home.html'},
+            'options@home': {templateUrl: 'sub-calcoptions.html'},
+            'menu@home': {templateUrl: 'sub-menu.html'},
+            'search@home': {templateUrl: 'sub-search.html'}
+        }
+    })
 
-     /*.state('home.helloworld', {
-        url: '/',
-        controller: 'HelloWorldCtrl',
-        templateUrl: 'helloworld.html'
-    });*/
+    // Map page with multiple named views
+    .state('map', {
+        url: '/map',
+        views:{
+            '': {templateUrl: 'page-map.html'},
+            'menu@map': {templateUrl: 'sub-menu.html'}
+        }
+    })
 
-        // homepage with multiple named views
-        .state('home', {
-            url: '/',
-            views: {
-                // HOMEPAGE
-                '': {templateUrl: 'page-home.html'},
-                'options@home': {templateUrl: 'sub-calcoptions.html'},
-                'menu@home': {templateUrl: 'sub-menu.html'},
-                'search@home': {templateUrl: 'sub-search.html'}
-            }
-        })
+    // landing questionnaire page with multiple views
+    .state('landingquestionnaire', {
+        url: '/infoquestionnaire',
+        views: {
+            '': {templateUrl: 'page-landing-questionnaire.html'},
+            'menu@landingquestionnaire': {templateUrl: 'sub-menu.html'}
+        }
+    })
 
-        // map page with multiple named views
-        .state('map', {
-            url: '/map',
-            views:{
-                '': {templateUrl: 'page-map.html'},
-                'menu@map': {templateUrl: 'sub-menu.html'}
-            }
-        })
+    // Questionnaire page with multiple views
+    .state('questionnaire', {
+        url: '/questionnaire',
+        views: {
+            '': {templateUrl: 'page-questionnaire.html', controller: 'QuestionCtrl'},
+            'menu@questionnaire': {templateUrl: 'sub-menu.html'}
+        }
+    })
 
-        // landing questionnaire page with multiple views
-        .state('landingquestionnaire', {
-            url: '/infoquestionnaire',
-            views: {
-                '': {templateUrl: 'page-landing-questionnaire.html'},
-                'menu@landingquestionnaire': {templateUrl: 'sub-menu.html'}
-            }
-        })
+    // Contact page with multiple views
+    .state('contact', {
+        url: '/contact',
+        views: {
+            '': {templateUrl: 'page-contact.html', controller: 'ContactCtrl'},
+            'menu@contact': {templateUrl: 'sub-menu.html'}
+        }
+    })
 
-        // questionnaire page with multiple views
-        .state('questionnaire', {
-            url: '/questionnaire',
-            views: {
-                '': {templateUrl: 'page-questionnaire.html'},
-                'menu@questionnaire': {templateUrl: 'sub-menu.html', controller: 'QuestionCtrl'}
-            }
-        })
+    // FAQ page with multiple views
+    .state('faq', {
+        url: '/veel-gestelde-vragen',
+        views:{
+            '': {templateUrl: 'page-faq.html', controller: 'FaqCtrl'},
+            'menu@faq': {templateUrl: 'sub-menu.html'}
+        }
 
-        // faq page with multiple views
-        .state('faq', {
-            url: '/veel-gestelde-vragen',
-            views:{
-                '': {templateUrl: 'page-faq.html'},
-                'menu@faq': {templateUrl: 'sub-menu.html'}
-            }
+    })
 
-        })
+    // Energytips page with multiple views
+    .state('energytips', {
+        url: '/energie-tips',
+        views:{
+            '': {templateUrl: 'page-energytips.html', controller: 'ExampleCtrl'},
+            'menu@faq': {templateUrl: 'sub-menu.html'}
+        }
+    })
 
-        // contact page with multiple views
-        .state('contact', {
-            url: '/contact',
-            views: {
-                '': {templateUrl: 'page-contact.html'},
-                'menu@contact': {templateUrl: 'sub-menu.html'}
-            }
-
-        });
 
 
   $urlRouterProvider.otherwise('/');
