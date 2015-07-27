a560817d2e0583701dcce68f444a2472143daf92
unknown <panny.alana@gmail.com>
2015-07-27 10:10:07 +0200

    added require('angular-resource')

--- app/js/services/_index.js
+++ app/js/services/_index.js
@@ -3,6 +3,6 @@
 var angular = require('angular');
 var bulk = require('bulk-require');
 
-module.exports = angular.module('app.services', []);
+module.exports = angular.module('app.services', [require('angular-resource')]);
 
 bulk(__dirname, ['./**/!(*_index|*.spec).js']);
