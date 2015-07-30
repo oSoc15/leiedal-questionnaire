b11d430aa793907b8a0afdecca9f3a1b276de8ee
unknown <panny.alana@gmail.com>
2015-07-30 14:46:57 +0200

    .post() and .get()

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,86 @@
 'use strict';
 
-angular.module('app', ['ngResource', 'ngStorage'])
+//angular.module('app', ['ngResource', 'ngStorage'])
+angular.module('app', [])
 
+/*.factory('Residence', ['$resource',
+	function($resource) {
+		return $resource('http://leiedal.app/api/residence/:reply', {
+			save:	{	method: 'POST'	}
+		});
+	}
+]);*/
+
+.controller('MainCtrl', ['$scope', '$http',
+	function($scope, $http) {
+
+		var residence;
+
+		// works get questions
+		$http.get("http://leiedal.app/api/questions/").success(function(data){
+			$scope.questions = data;
+			console.log(data);
+		});
+
+		$http.post("http://leiedal.app/api/residences/reply", { 'city': 'Kortrijk', 'postalCode': 8500, 'street': 'Grote Markt', 'number': 54 })
+			.success(function(data, status, headers, config){
+				$scope.residence(data);
+			});
+
+
+		/*var data = ('Kortrijk', 8500, 'Grote Markt', 54);
+
+		$http.post('http://leiedal.app/api/residences/reply', data).success(successCallback);
+
+		console.log(successCallback);*/
+
+		/*$http.post('http://leiedal.app/api/residences/reply',
+			{ city: 'Kortrijk', postalCode: 8500, street: 'Grote Markt', number: 54}).success(function(data){
+
+			}).error(function(data){
+
+			});*/
+
+		/*$http.get("http://leiedal.app/api/questions/")
+			.success(functionsuccessQuestions){
+				console.log(successQuestions);
+			}
+		$scope.questions = Questions.query();
+
+		console.log($scope.questions)*/
+
+
+
+		/*var residence = $resource("http://leiedal.app/api/residences/reply", null, { save: {method: "POST"}});
+
+		residence.save(
+			{
+				city: 'Kortrijk',
+				postalCode: 8500,
+				street: 'Grote Markt',
+				number: 54
+			}
+		);
+
+
+		var Questions = $resource("http://leiedal.app/api/questions/");
+		$scope.questions = Questions.query();
+
+		console.log($scope.questions);*/
+
+		//var residence = new ResidenceService();
+		//$scope.residence = new residences();
+
+		/*$scope.residence = {};
+
+		$scope.createResidence = function(){
+			residence.$save();
+    	}*/
+	}
+]);
+
+
+/*
 .controller('MainCtrl', ['$scope', '$resource', '$localStorage',
 	function($scope, $resource, $localStorage) {
 
@@ -38,21 +117,42 @@ angular.module('app', ['ngResource', 'ngStorage'])
 
 			// Next question
 			if((answer.type||question.type)!=='year')
-			$scope.q++;
+				$scope.q++;
 
 
 		};
 
-		// Calculate energielabel
+		// Energylabels with their properties.
+		var labels = [
+						{ 'title': 'A', 'minRange': 600, 'maxRange': 699, 'color': '#05A157' },
+						{ 'title': 'B', 'minRange': 500, 'maxRange': 599, 'color': '#47B846' },
+						{ 'title': 'C', 'minRange': 400, 'maxRange': 499, 'color': '#B9D800' },
+						{ 'title': 'D', 'minRange': 300, 'maxRange': 399, 'color': '#FEDC00' },
+						{ 'title': 'E', 'minRange': 200, 'maxRange': 299, 'color': '#FFB700' },
+						{ 'title': 'F', 'minRange': 100, 'maxRange': 199, 'color': '#FF6E03' },
+						{ 'title': 'G', 'minRange': 0, 	 'maxRange': 99,  'color': '#F00001' }
+					];
+
+		// Calculate energylabel
 		$scope.label = function() {
 
+			// Anwser of the residence
 			var a = $scope.stor.r.answers;
+
+			// Initialize the score
 			var score = 0;
 
-			// floors
+			// SCORES
+			// ======
+			// - size
+			score += 100-(a.size||1)*30;
+
+			// - floors
 			score += 100-(a.floors||2)*30;
+
+			score += 100-(a.basement||1)*30;
 
 			return score;
 		};
 	}
-]);
+]);*/
